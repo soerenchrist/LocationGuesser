6e35f820fa57030305718bfc89896c29c90ff59f
Sören Christ <s.christ@mailbox.org>
2023-04-18 09:18:11 +0000

    Add validation for cosmos options

--- src/LocationGuesser.Core/Options/CosmosDbOptions.cs
+++ src/LocationGuesser.Core/Options/CosmosDbOptions.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocationGuesser.Core.Options;
 
 public class CosmosDbOptions
 {
+    [Required]
     public string Endpoint { get; set; } = string.Empty;
+    [Required]
     public string DatabaseName { get; set; } = string.Empty;
+    [Required]
     public string ContainerName { get; set; } = string.Empty;
 }
